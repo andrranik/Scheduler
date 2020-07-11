e76a0d222e076835c39b6dd1e02d4e5a156edb75
Andranik Aleksanyan <andoalex1997@gmail.com>
2020-07-11 15:15:02 +0300

    Add project files.

---
+++ Scheduler.Service/Controllers/WorkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scheduler.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
